3538602d3a4de857a143f1c9d36892abed7fda0f
BaronLiu1993 <baronliu1993@gmail.com>
2023-08-27 01:42:42 -0400

    Update Information.jsx

--- src/components/information/Information.jsx
+++ src/components/information/Information.jsx
@@ -3,7 +3,7 @@ import './information.css'
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose"
 import Webcam from "react-webcam"
-import { drawHand } from "./utilities"
+import {drawHand} from "./utilities"
 import * as fp from "fingerpose";
 import victory from "./victory.png";
 import thumbs_up from "./thumbs_up.png";
@@ -16,57 +16,45 @@ function Information() {
   const images = { thumbs_up: thumbs_up, victory: victory }; 
 
   const runHandpose = async () => {
-    const net = await handpose.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
+    const net = await handpose.load()
+    console.log('Loaded Handpose Model')
     setInterval(() => {
-      detect(net);
+      detect(net)
     }, 10);
+
   };
 
   const detect = async (net) => {
-
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-
-      const video = webcamRef.current.video;
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState ===4) {
+      const video =webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.width =videoWidth
       webcamRef.current.video.height = videoHeight;
 
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
       const hand = await net.estimateHands(video);
+      console.log(hand);
+
       if (hand.length > 0) {
         const GE = new fp.GestureEstimator([
           fp.Gestures.VictoryGesture,
-          fp.Gestures.ThumbsUpGesture,
+          fp.Gestures.ThumbsUpGesture, 
         ]);
-        const gesture = await GE.estimate(hand[0].landmarks, 4);
-        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
-          const confidence = gesture.gestures.map(
-            (prediction) => prediction.confidence
-          );
-          const maxConfidence = confidence.indexOf(
-            Math.max.apply(null, confidence)
-          );
-          setEmoji(gesture.gestures[maxConfidence].name);
-          console.log(emoji);
-        }
+
+        const gesture = await GE.estimate(hand[0].landmarks, 8);
+        console.log(gesture);
       }
+
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
     }
   };
 
-
-  useEffect(()=>{runHandpose()},[]);
+  runHandpose();
 
   return (
     <>
@@ -78,15 +66,15 @@ function Information() {
               zindex: 9,
               width: 1000,
               height: 800,
-              right: 845
+              right: 840
             }} />
             <canvas ref = {canvasRef}
             style = {{
               position: "absolute",
               zindex: 9,
-              width: 2000,
+              width: 1000,
               height: 800,
-              right: 500
+              right: 840
             }}/>
             {emoji !== null ? (
           <img
