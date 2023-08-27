cc9fdd8f76753a166d8d4b7b2f4f4400222d8f37
Rachel Wei <143151417+rachelqrwei@users.noreply.github.com>
2023-08-27 11:11:28 -0400

    Add files via upload

---
+++ Ar_Glasses.ino
@@ -0,0 +1,27 @@
+int currentState;
+int lastState = LOW;
+
+void setup() {
+  pinMode(0, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int xValue = 0;
+  int yValue = 0;
+  currentState = digitalRead(0);
+
+  if(digitalRead(0)) 
+    Serial.println("The sensor is touched");
+
+  lastState = currentState;
+  xValue = analogRead(A0);
+  yValue = 1023 - analogRead(A1);
+  Serial.print('(');
+  Serial.print(xValue);
+  Serial.print(", ");
+  Serial.print(yValue);
+  Serial.println(')');
+}
